5118368a91bcfb3419578dc350a375f28a9dd800
suryamishra007 <74192796+suryamishra007@users.noreply.github.com>
2021-03-27 11:02:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+
+var divisions = [];
+var plinkos = [];
+
+var score = 0;
+var particle;
+var count = 0;
+
+var t;
+
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  t = new Ground(400,500,20,20)
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+
+  
+
+  textSize(20)
+ text("Score : "+score,20,30);
+
+ text("500",25,600);
+ text("500",100,600);
+ text("500",175,600);
+ text("500",250,600);
+
+ text("100",340,600);
+ text("100",425,600);
+ text("100",500,600);
+
+ text("200",575,600);
+ text("200",650,600);
+ text("200",750,600);
+
+  Engine.update(engine);
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+   t.display();
+   
+
+   
+}
